a93d465676c766642b54f8dc36ba40aae2f47998
Lorena Valle <lore1268valle@gmail.com>
2024-07-30 20:12:46 -0600

    pass stage

--- app/server.go
+++ app/server.go
@@ -41,29 +41,43 @@ func handleConnection(conn net.Conn) {
 	lines := strings.Split(string(buf), "\r\n")
 	method := strings.Split(lines[0], " ")[0]
 	pathFile := strings.Split(lines[0], " ")[1]
+	acceptEncoding := ""
+
+	for _, line := range lines {
+		if strings.HasPrefix(line, "Accept-Encoding:") {
+			acceptEncoding = strings.TrimSpace(strings.Split(line, ":")[1])
+			break
+		}
+	}
 
 	if method == "GET" && pathFile == "/" {
 		response = "HTTP/1.1 200 OK\r\n\r\n"
-	} else if method == "GET" && pathFile[0:6] == "/echo/" {
-		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:%d\r\n\r\n%s", len(pathFile[6:]), pathFile[6:])
+	} else if method == "GET" && strings.HasPrefix(pathFile, "/echo/") {
+		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(pathFile[6:]), pathFile[6:])
 	} else if method == "GET" && pathFile == "/user-agent" {
 		ua := strings.Split(lines[2], " ")[1]
-		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:%d\r\n\r\n%s", len(ua), ua)
-	} else if method == "GET" && pathFile[0:7] == "/files/" {
+		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(ua), ua)
+	} else if method == "GET" && strings.HasPrefix(pathFile, "/files/") {
 		dir := os.Args[2]
 		content, err := os.ReadFile(path.Join(dir, pathFile[7:]))
 		if err != nil {
 			response = "HTTP/1.1 404 Not Found\r\n\r\n"
 		} else {
+			//STAGE 9
 			response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(content), string(content))
 		}
-	} else if method == "POST" && pathFile[0:7] == "/files/" {
+	} else if method == "POST" && strings.HasPrefix(pathFile, "/files/") {
 		content := strings.Trim(lines[len(lines)-1], "\x00")
 		dir := os.Args[2]
 		_ = os.WriteFile(path.Join(dir, pathFile[7:]), []byte(content), 0644)
 		response = "HTTP/1.1 201 Created\r\n\r\n"
 	} else {
 		response = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+
+	//STAGE 9
+	if acceptEncoding == "gzip" {
+		response = strings.Replace(response, "\r\n\r\n", "\r\nContent-Encoding: gzip\r\n\r\n", 1)
 	}
 
 	conn.Write([]byte(response))
